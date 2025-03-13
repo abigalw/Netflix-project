00d4da136417c223907cb68012590104510686a1
abigalw <abigalw14@gmail.com>
2025-03-12 22:00:47 -0600

    vite fix 3

--- Netflix-Clone/vite.config.js
+++ Netflix-Clone/vite.config.js
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/Netflix-Clone/",
   plugins: [react()],
-  root: "." ,
+  root: "./" ,
+  base: "/",
   build: { outDir: "dist", }
 })
